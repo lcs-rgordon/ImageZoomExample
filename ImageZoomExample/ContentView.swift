93cd16ed45baf037bad30046abe14ba266790841
Russell Gordon <github@russellgordon.ca>
2022-02-24 07:05:51 -0500

    Image zoom example.

--- ImageZoomExample/ContentView.swift
+++ ImageZoomExample/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    // Controls whether the image is zoomed in or not
+    @State var zoomed = false
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+//        Image("orange-flowers")
+//            .resizable()
+//            // Same as ".scaledToFit()"
+//            .aspectRatio(contentMode: .fit)
+        
+//        Image("orange-flowers")
+//            .resizable()
+//            // Same as ".scaledToFill()"
+//            .aspectRatio(contentMode: .fill)
+        
+        Image("orange-flowers")
+            .resizable()
+            // When zoomed == true, zoom in
+            .aspectRatio(contentMode: zoomed ? .fill : .fit)
+            // Change (toggle) zoom mode when tapped
+            .onTapGesture {
+                withAnimation {
+                    zoomed.toggle()
+                }
+            }
+        
     }
 }
 
