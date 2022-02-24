4aec1e1d7b4b1d8f5ab801153cda9fef601ae27a
Russell Gordon <github@russellgordon.ca>
2022-02-24 06:54:12 -0500

    Initial Commit

---
+++ ImageZoomExample/ImageZoomExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageZoomExampleApp.swift
+//  ImageZoomExample
+//
+//  Created by Russell Gordon on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ImageZoomExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
